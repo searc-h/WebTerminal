6d605d4ce336269bb9db1eff78899cd7c27c99de
searc-h <2583448925@qq.com>
2022-10-13 12:14:27 +0800

    updated the timing Component

--- src/components/TIming/Timing.vue
+++ src/components/TIming/Timing.vue
@@ -1,7 +1,24 @@
 <template>
-    Timing
+    {{currenTime}}
 </template>
 <script setup lang="ts">
+import Dayjs from '../../utils/myDayjs'
+import {onMounted , onUnmounted , ref} from 'vue'
+
+let currenTime = ref<any>("") 
+let timer:any = null
+let changeTime = ()=>{
+    console.log("changeTime")
+    currenTime.value =  Dayjs(new Date()).format("MM-DD HH : mm")
+    timer =  setTimeout( changeTime ,10000)
+}
+onMounted(()=>{
+    changeTime()
+})
+
+onUnmounted(()=>{
+    clearTimeout(timer)
+})
 
 </script>
 <style>
