ca5e3826423a3f0c7ca231a0d053b924096dd836
searc-h <2583448925@qq.com>
2023-01-03 14:12:05 +0800

    fix the bg command

--- src/core/commands/terminal/config/backgroundCommand.ts
+++ src/core/commands/terminal/config/backgroundCommand.ts
@@ -5,7 +5,7 @@ import myAxios from "../../../../utils/myAxios"
 export const backgroundCommand :CommandType = {
     func : "background",
     name : "背景",
-    desc : "切换背景图片",
+    desc : "切换背景图片（不再支持）",
     alias : ["bg","back"],
     options : [],
     params :[
@@ -28,11 +28,13 @@ export const backgroundCommand :CommandType = {
             return
         }
         if(_.length<1){
-            let result:any = await myAxios.get("/backgroundRandom")
-            if(result.error==0)
-                TerminalStore.setBackground(result.data)
-            else
-                terminal.writeTextErrorResult("获取失败")
+            // let result:any = await myAxios.get("/backgroundRandom")
+            // if(result.error==0)
+            //     TerminalStore.setBackground(result.data)
+            // else
+                terminal.writeTextResult("由于存在安全隐患，现已不支持随机获取图片", "info")
+                terminal.writeTextResult("你可以通过点击右上角抽屉进行本地图片选择或者输入在线图片地址", "info")
+                
         }
     }
 }
